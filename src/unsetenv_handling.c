0051b9d3b9c73415b02ff3c58165c8ed91818ac4
planch2 <planch2@localhost.localdomain>
2018-04-11 12:21:15 +0200

    adding unsetenv

--- src/unsetenv_handling.c
+++ src/unsetenv_handling.c
@@ -8,11 +8,27 @@
 #include "my.h"
 #include "minishell2.h"
 
+void main_unsetenv(svar_t *svar, char *command)
+{
+	char *t_arg;
+	int nbr_args = count_chr(command, ' ');
+
+	if (nbr_args < 1) {
+		my_putstrror("unsetenv: Too few arguments.\n");
+		svar->returnv = 1;
+		return;
+	} for (int i = 0; i < nbr_args; i++) {
+		t_arg = get_args_nbr(command, i + 1);
+		svar->c_env = my_tbl_rm(svar->c_env, search_env(t_arg, svar));
+		free(t_arg);
+	}
+}
+
 int verify_unsetenv_command(svar_t *svar, char *command)
 {
 	if (!verify_command(command, "unsetenv"))
 		return (0);
-	my_putstr("unsetenv\n");
 	svar->returnv = 0;
+	main_unsetenv(svar, command);
 	return (1);
 }
