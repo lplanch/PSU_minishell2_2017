77a23b232e64973039f1124cf0b4d383c3cc38d3
planch2 <planch2@localhost.localdomain>
2018-04-15 19:00:17 +0200

    closing one after one

--- src/redirect_fork_commands.c
+++ src/redirect_fork_commands.c
@@ -18,10 +18,8 @@ void pipe_command(svar_t *svar, char *command, int *arr_fd, int *iter)
 			dup2(arr_fd[iter[0] * 2 - 2], 0);
 		if (iter[0] < iter[1] - 1)
 			dup2(arr_fd[iter[0] * 2 + 1], 1);
-		for (int i = 0; i < iter[1] * 2 - 2; i += 2) {
+		for (int i = 0; i < iter[1] * 2 - 1; i++)
 			close(arr_fd[i]);
-			close(arr_fd[i + 1]);
-		}
 		verify_user_command(svar, command);
 		exit(svar->returnv);
 	}
